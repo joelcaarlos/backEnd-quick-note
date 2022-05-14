b0ae2d8eba4bac2c6af1ddf36235befa9cab7087
joel costa <joelcosta@cpejr.com.br>
2022-05-14 16:09:34 -0300

    primeiros arquivos

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const port = process.env.PORT ||3333;
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(port, ()=>{
+    console.log('listening on port ' + port);
+});
